27b7d023934e54cdc8ec5197c713a9ff20d534e0
Mina <cccoldatlas@gmail.com>
2022-08-24 09:05:23 +0200

    update JS file and edit readme

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Landing Page Project
 
-## Table of Contents
+## Description
 
-* [Instructions](#instructions)
+The first landing page project in the Professional track.
 
-## Instructions
+## Author: Mina Anwar
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## Techs:
+1. HTML5
+2. CSS
+3. Javascript
--- js/app.js
+++ js/app.js
@@ -37,8 +37,8 @@
  * Begin Main Functions
  * 
 */
-//const sections = document.querySelectorAll('section');
 
+// build the nav
 const sections = document.querySelectorAll('section');
 const ul = document.getElementById('navbar__list');
 const fragment = document.createDocumentFragment();
@@ -51,34 +51,39 @@ sections.forEach(function (section) {
     link.href = '#'+sectionId; // "#"+ section Id;
     link.textContent = sectionTitle;
     link.classList.add('menu__link');
+    // Scroll to section on link click
     link.addEventListener('click', (e) => {
         e.preventDefault();
+        // Scroll to anchor ID using scrollTO event
         section.scrollIntoView({
             behavior: 'smooth',
         });
+        
     });
     li.appendChild(link);
     fragment.appendChild(li);
 });
 ul.appendChild(fragment);
 
-// build the nav
-
-
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
+const callbackObserverFunction = entries =>{
+    const activeLink = document.querySelector('a[href="#${entries[0].target.id}"]');
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("your-active-section");
+    }else{
+        entries[0].target.classList.remove("your-active-section");
+        activeLink.classList.remove('active');
+    }
+}
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.35
+}
+const observer = new IntersectionObserver(callbackObserverFunction, options);
+window.addEventListener('scroll', function(){
+    sections.forEach(section =>{
+        observer.observe(section);
+    })
+})
 // Set sections as active
